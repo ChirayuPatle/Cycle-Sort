29afd2af57cd93376145a2c13fbf39e9c48aea6a
Chirayupatle <chirayu.patle@vectorlab.in>
2026-03-16 21:07:50 +0530

    cycle sort

---
+++ src/CyclicSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr){
+       // create pointer
+        int i =0;
+
+        while(i < arr.length){
+            int correct = arr[i] - 1; // Position of element
+            if(arr[i] != arr[correct]){
+                // traverse for other element
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] =temp;
+            }
+            else{
+                // swap elements when element is not at correct index
+                i++;
+            }
+        }
+    }
+}
