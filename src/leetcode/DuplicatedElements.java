aea4490bb48d51181477e2a17952fe3b43fd012a
Chirayupatle <chirayu.patle@vectorlab.in>
2026-03-17 21:29:28 +0530

    practice problems

--- src/leetcode/DuplicatedElements.java
+++ src/leetcode/DuplicatedElements.java
@@ -4,12 +4,9 @@ import java.util.Arrays;
 
 public class DuplicatedElements {
     public static void main(String[] args) {
-//        int[] arr = {5,4,3,2,1};
         int[] arr = {1,3,4,2,2};
         int res = sort(arr);
         System.out.println(res);
-//        sort(arr);
-//        System.out.println(Arrays.toString(arr));
     }
 
     static int sort(int[] nums){
